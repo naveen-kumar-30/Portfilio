ed7b29f4cc36c6506ee1a5f88ee2d265b2dec552
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-03-31 12:59:48 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
-
+import image from '../assets/NaveenP.jpg'
 export const headerData = {
     name: 'NaveenKumar ',
     title: "Web Developer",
     desciption:"Passionate about leveraging my technical and interpersonal skills to deliver innovative software solutions. Interested in Cloud Computing, UI/UX, Networking, and AI & ML. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: image,
     resumePdf: resume
 }
