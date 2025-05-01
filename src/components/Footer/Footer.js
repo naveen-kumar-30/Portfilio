a5b299a452334fa795973e2eddeccf3d0e69232f
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-05-01 22:32:20 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import { headerData } from '../../data/headerData'
 function Footer() {
 
     const shortname = (name) => {
-        if(name.length > 10) {
+        if(name.length > 16) {
             return name.split(" ")[0]
         } else {
             return name
@@ -17,7 +17,13 @@ function Footer() {
 
     return (
         <div className="footer" style={{backgroundColor: theme.secondary}}>
-                <p> </p>
+            {/* <p style={{color: theme.tertiary}}>
+                Made with  
+                <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
+                    ❤
+                </span>
+                 by {shortname(headerData.name)}
+            </p> */}
         </div>
     )
 }
